dd20a23d65a2dc9124133ac1f926abc8ce5797ab
Haseebrb <81767951+ah3653070@users.noreply.github.com>
2021-08-19 13:08:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ ADL_BOT_RQ = {}
 START_TEXT = """
 Hello {} , I'am a simple file or media rename bot with permanent thumbnail support.
 
-Made by @FayasNoushad
+© @FayasNoushad
 """
 HELP_TEXT = """
 <b><u>Rename</u></b>
